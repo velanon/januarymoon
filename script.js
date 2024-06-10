3fc548a1f50d7c188d79847833a80b04b58616e5
velanon <taraflores0410@gmail.com>
2024-06-11 00:57:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById('password-form').addEventListener('submit', function(event) {
     event.preventDefault();
     
-    const password = document.getElementById('c0c7c76d30bd3dcaefc96f40275bdc0a');
+    const correctPasswordHash = 'c0c7c76d30bd3dcaefc96f40275bdc0a'; 
+    const password = document.getElementById('password').value; 
     const hashedPassword = CryptoJS.SHA256(password).toString();
-    console.log(hashedPassword);
 
-    if (hashedPassword === 'c0c7c76d30bd3dcaefc96f40275bdc0a') {
-        window.location.href = 'menu.html';
+    if (hashedPassword === 'correctPasswordHash') {
+        window.location.href = 'menu.html'; // Redirect to the menu page if the password is correct
     } else {
         alert('Incorrect password. Please try again. I know you can remember it.');
     }
