d7a3b6b928957513cfd8e004dafd8b3a9eea7986
velanon <taraflores0410@gmail.com>
2024-06-10 14:20:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('password-form').addEventListener('submit', function(eve
     event.preventDefault();
     
     const password = document.getElementById('password').value;
-    const correctPassword = '01/14/24'; // Replace with your actual password
+    const correctPassword = '01/14/24'; 
 
     if (password === correctPassword) {
         document.getElementById('password-form').classList.add('hidden');
