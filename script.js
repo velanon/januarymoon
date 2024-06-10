d393348133fdc8a23c7006d5acd6c610da44446a
velanon <taraflores0410@gmail.com>
2024-06-11 00:22:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,34 +2,13 @@ document.getElementById('password-form').addEventListener('submit', function(eve
     event.preventDefault();
     
     const password = document.getElementById('password').value;
-    const correctPasswordHash = 'c0c7c76d30bd3dcaefc96f40275bdc0a'; // SHA-256 hash of 'toad011424aurora'
+    const correctPasswordHash = 'c0c7c76d30bd3dcaefc96f40275bdc0a'; 
 
-    const hashedPassword = hashPassword(password);
+    const hashedPassword = CryptoJS.SHA256(password).toString();
 
     if (hashedPassword === correctPasswordHash) {
-        incrementPasswordCount();
-        updatePasswordCountDisplay();
         window.location.href = 'menu.html'; // Redirect to the menu page
     } else {
-        alert('Incorrect password. Please try again.');
+        alert('Incorrect password. Please try again. I know you can remember it.');
     }
 });
-
-function hashPassword(password) {
-    return CryptoJS.SHA256(password).toString();
-}
-
-function incrementPasswordCount() {
-    let passwordCount = localStorage.getItem('passwordCount');
-    if (!passwordCount) {
-        localStorage.setItem('passwordCount', '0');
-        passwordCount = 0;
-    }
-    passwordCount++;
-    localStorage.setItem('passwordCount', passwordCount.toString());
-}
-
-function updatePasswordCountDisplay() {
-    const passwordCount = localStorage.getItem('passwordCount');
-    document.getElementById('password-count').innerText = `Password entered ${passwordCount} times.`;
-}
