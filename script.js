decd6e2a67f3225762dc51b2c22b409f418c72be
velanon <taraflores0410@gmail.com>
2024-06-11 00:12:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-
 document.getElementById('password-form').addEventListener('submit', function(event) {
     event.preventDefault();
     
     const password = document.getElementById('password').value;
-    const correctPasswordHash = 'c0c7c76d30bd3dcaefc96f40275bdc0a'; 
+    const correctPasswordHash = 'c0c7c76d30bd3dcaefc96f40275bdc0a';
 
     const hashedPassword = hashPassword(password);
-    console.log(`Entered password: ${password}`);
-    console.log(`Hashed password: ${hashedPassword}`);
-    console.log(`Correct hash: ${correctPasswordHash}`);
 
     if (hashedPassword === correctPasswordHash) {
         incrementPasswordCount();
         updatePasswordCountDisplay();
-        window.location.href = 'menu.html'; // Redirect to the menu page
+        window.location.href = 'menu.html';
     } else {
         alert('Incorrect password. Please try again. I know you can remember it.');
     }
 });
 
-// Function to hash the password using SHA-256
 function hashPassword(password) {
     return CryptoJS.SHA256(password).toString();
 }
 
-// Function to increment the password count
 function incrementPasswordCount() {
     let passwordCount = localStorage.getItem('passwordCount');
     if (!passwordCount) {
@@ -33,10 +27,8 @@ function incrementPasswordCount() {
     }
     passwordCount++;
     localStorage.setItem('passwordCount', passwordCount.toString());
-    console.log(`Password entered ${passwordCount} times.`);
 }
 
-// Function to update the display of password count
 function updatePasswordCountDisplay() {
     const passwordCount = localStorage.getItem('passwordCount');
     document.getElementById('password-count').innerText = `Password entered ${passwordCount} times.`;
