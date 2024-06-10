a9699bea5d3ee705728a8a6e89bc847db65e2536
velanon <taraflores0410@gmail.com>
2024-06-11 01:15:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-document.getElementById('password-form').addEventListener('submit', function(event) {
+ document.getElementById('password-form').addEventListener('submit', function(event) {
     event.preventDefault();
     
-    const correctPasswordHash = 'c0c7c76d30bd3dcaefc96f40275bdc0a';
-    const password = document.getElementById('password').value;
-    const hashedPassword = CryptoJS.SHA256(password).toString();
+    const correctPasswordHash = 'c0c7c76d30bd3dcaefc96f40275bdc0a'; // Correct hashed value of the password
+    const password = document.getElementById('password'); // Get the value of the password input field
+    const hashedPassword = CryptoJS.SHA256(password).toString(); // Hash the entered password
 
-    if (hashedPassword === 'c0c7c76d30bd3dcaefc96f40275bdc0a') {
+    console.log('Entered Password:', password);
+    console.log('Hashed Password:', hashedPassword);
+    console.log('Correct Password Hash:', correctPasswordHash);
+
+    if (hashedPassword === correctPasswordHash) {
+        console.log('Password is correct. Redirecting to menu.html...');
         window.location.href = 'menu.html'; // Redirect to the menu page if the password is correct
     } else {
         alert('Incorrect password. Please try again. I know you can remember it.');
