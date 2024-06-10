c565f8aefd0bd4d6bc14457b4f32d4154f175d9f
velanon <taraflores0410@gmail.com>
2024-06-11 00:13:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ document.getElementById('password-form').addEventListener('submit', function(eve
     event.preventDefault();
     
     const password = document.getElementById('password').value;
-    const correctPasswordHash = 'c0c7c76d30bd3dcaefc96f40275bdc0a';
+    const correctPasswordHash = 'c0c7c76d30bd3dcaefc96f40275bdc0a'; // SHA-256 hash of 'toad011424aurora'
 
     const hashedPassword = hashPassword(password);
 
     if (hashedPassword === correctPasswordHash) {
         incrementPasswordCount();
         updatePasswordCountDisplay();
-        window.location.href = 'menu.html';
+        window.location.href = 'menu.html'; // Redirect to the menu page
     } else {
-        alert('Incorrect password. Please try again. I know you can remember it.');
+        alert('Incorrect password. Please try again.');
     }
 });
 
