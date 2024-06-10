1d01fbb5a7885cc31a5ae050c673e0990ad55352
velanon <taraflores0410@gmail.com>
2024-06-11 00:00:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ document.getElementById('password-form').addEventListener('submit', function(eve
     const password = document.getElementById('password').value;
     const correctPasswordHash = 'c0c7c76d30bd3dcaefc96f40275bdc0a'; 
 
-    if (hashPassword(password) === correctPasswordHash) {
+    const hashedPassword = hashPassword(password);
+    console.log(`Entered password: ${password}`);
+    console.log(`Hashed password: ${hashedPassword}`);
+    console.log(`Correct hash: ${correctPasswordHash}`);
+
+    if (hashedPassword === correctPasswordHash) {
         incrementPasswordCount();
         updatePasswordCountDisplay();
         window.location.href = 'menu.html'; // Redirect to the menu page
